58bd8d2579d635068a503f9e0cd5addbdea30a19
AlissonLopes <alisson.silvalopes@gmail.com>
2026-03-29 01:04:46 -0300

    adding classes generated from StarUML

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+#I call library
 import pygame
 
+print('setup start')
 pygame.init()
 window = pygame.display.set_mode(size=(800, 600))
+print('setup end')
 
+print('setup loopin')
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
